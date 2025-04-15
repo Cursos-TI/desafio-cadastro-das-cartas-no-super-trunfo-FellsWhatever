52a0cb208d91f3825bb7c5ee46750d248bd22fcf
FellsWhatever <felipepcordeiro7@gmail.com>
2025-04-15 15:52:06 +0000

    terminei o codigo da segunda carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,6 +9,14 @@ int main() {
     float PIB;
     int PontosTuristicos;
 
+    char Estado_B = 'B';
+    char Codigo_B[20] = "B02";
+    char Cidade_B[50] = "SaoPaulo";
+    int Populacao_B;
+    float Area_B;
+    float PIB_B;
+    int PontosTuristicos_B;
+
     printf("Sua cidade é: %s\n", Cidade);
 
     printf("Digite a população da sua cidade: \n");
@@ -25,6 +33,23 @@ int main() {
     printf("a Área de sua cidade é: %.2f\n", Area);
     printf("o PIB da sua cidade é: %.2f\n", PIB);
     printf("e a quantidade de pontos turisticos da sua cidade é: %d\n", PontosTuristicos);
+
+    printf("Sua nova cidade é: %s\n", Cidade_B);
+
+    printf("Digite a população da sua cidade: \n");
+    scanf("%d", &Populacao_B);
+    printf("Digite a Área de sua cidade: \n");
+    scanf("%f", &Area_B);
+    printf("Digite o PIB de sua cidade: \n");
+    scanf("%f", &PIB_B);
+    printf("Digite a quantidade de pontos turisticos da sua cidade: \n");
+    scanf("%d", &PontosTuristicos_B);
+
+    printf("Sua cidade é: %s\n", Cidade_B);
+    printf("ela possui a população de: %d\n", Populacao_B);
+    printf("a Área de sua cidade é: %.2f\n", Area_B);
+    printf("o PIB da sua cidade é: %.2f\n", PIB_B);
+    printf("e a quantidade de pontos turisticos da sua cidade é: %d\n", PontosTuristicos_B);
 
     return 0;
 }
